9970dc312c5a17906fd1d047e795804fe8defcb7
Cristian Andreoli <cristian.andreoli01@universitadipavia.it>
2019-05-16 21:22:32 +0200

    Clean the code

--- app/src/main/java/com/example/gateway/MainActivity.java
+++ app/src/main/java/com/example/gateway/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -20,31 +19,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ExpandableListView;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.gateway.adapters.BleGattProfileListAdapter;
 import com.example.gateway.ble.BleCommManager;
 import com.example.gateway.ble.BlePeripheral;
 import com.example.gateway.ble.callbacks.BleScanCallbackv18;
 import com.example.gateway.ble.callbacks.BleScanCallbackv21;
 import com.example.gateway.models.BlePeripheralListItem;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final UUID Battery_Service_UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb"); //Servizio
-    private static final UUID Battery_Level_UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb"); //Caratteristica
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int MULTIPLE_PERMISSIONS = 42;
@@ -55,152 +46,29 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.BLUETOOTH_ADMIN,
             Manifest.permission.ACCESS_FINE_LOCATION
     };
-
-    /**
-     * Bluetooth	Stuff
-     **/
     private BleCommManager mBleCommManager;
     private	boolean	mScanningActive	= false;
 
     private	MenuItem mScanProgressSpinner;
     private	MenuItem mStartScanItem, mStopScanItem;
-    //private ListView mBlePeripheralsListView;
     private ExpandableListView mBlePeripheralsListView;
     private TextView mPeripheralsListEmptyTV;
     private BleGattProfileListAdapter mBlePeripheralsListAdapter;
-
     private GestioneDB db;
-
-    //mio debug
-    private TextView textView;
-    //
-
-    //PROVE MIE
-    //---------------------------------------------------------------------------------------------------------------------------------------
-
     private Map<Integer, BlePeripheral> listaSensori = new HashMap<>();
     private ArrayList<BluetoothDevice> disposit = new ArrayList<>();
-    private BleGattProfileListAdapter.GroupViewHolder h;
-
-    /*public void onBleConnected(){
-        View porco = listaSensori.get(0).getV();
-        h = new BleGattProfileListAdapter.GroupViewHolder();
-        h.connection_Status.setText("SI");
-        porco.setTag(h);
-        //Log.v(TAG,"OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO   =   "+listaSensori.get(0).getConnessoStatus().toString());
-        //listaSensori.get(listaSensori.get(0).getDeviceId()).getConnessoStatus().setText("SI");
-        //Log.v(TAG,"OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO   =   "+listaSensori.get(0).getConnessoStatus().toString());
-
-        //listItem = parente.getChildAt(0);
-        //nuovavista = (BleGattProfileListAdapter.GroupViewHolder) listItem.getTag();
-        //nuovavista.connection_Status.setText("SI");
-        //listItem.setTag(nuovavista);
-    }*/
-
-    BleGattProfileListAdapter.GroupViewHolder nuovavista;
-
-    //private boolean mBleConnected;
-
-    /*private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                Log.v(TAG, "Connected to peripheral");
-                mBleConnected = true;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        onBleConnected();
-                    }
-                });
-                gatt.discoverServices();
-            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                mBlePeripheral.close();
-                mBleConnected = false;
-            }
-
-
-        }
-    };*/
-
-    public void prova(View view){
-        updateTextView(String.valueOf(mBlePeripheralsListView.getSelectedId())+"  "+String.valueOf(mBlePeripheralsListView.getSelectedPosition()));
-        Log.e(TAG,String.valueOf(mBlePeripheralsListView.getSelectedId())+"  "+String.valueOf(mBlePeripheralsListView.getSelectedPosition())+"  "+String.valueOf(mBlePeripheralsListView.getId()));
-    }
-
-
-    private int position;
-    private long id;
-    private BleGattProfileListAdapter.GroupViewHolder holder;
-    private ExpandableListView parente;
-    private View listItem;
     private int count;
-
-    /*public	void	attachCallbacks()	{
-
-        mBlePeripheralsListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-
-                return false;
-            }
-        });
-        //	if	a	list	item	is	clicked,
-        //	open	corresponding	Peripheral	in	the	ConnectActivity
-        mBlePeripheralsListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                Log.e(TAG,groupPosition+"  "+ id);
-                Log.e(TAG,"LISTA MAPPA =  "+ listaSensori.get(groupPosition));
-                if(listaSensori.get(groupPosition) == null){
-                    BlePeripheral blePeripheral = new BlePeripheral(disposit.get(groupPosition), gattCallback, getApplicationContext());
-                    blePeripheral.setDeviceId(groupPosition);
-                    listaSensori.put(groupPosition,blePeripheral);
-                }
-                Log.e(TAG,"NUOVAVISTA =  "+listaSensori.get(groupPosition).getVistaPadre());
-                if(listaSensori.get(groupPosition).getVistaPadre() == null){
-                    View listItem;
-                    listItem = parent.getChildAt(groupPosition);
-                    Log.e(TAG,"TAG =  "+listItem.getTag().toString());
-                    listaSensori.get(groupPosition).setVistaPadre((BleGattProfileListAdapter.GroupViewHolder) listItem.getTag());
-                }
-                //parente = parent;
-                return false;
-            }
-        });
-
-    }*/
-
-    public ImageView green;
     Activity a;
-
-
-    //---------------------------------------------------------------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
         checkPermissions();
         inizializzaUI();
-
-        //textView = findViewById(R.id.textView);
-        //textView.setMovementMethod(new ScrollingMovementMethod());
-
-        //attachCallbacks();
-
         db = new GestioneDB(this);
         db.open();
-
-        //green = (ImageView)findViewById(R.id.green_red_light);
-        //green.setColorFilter(Color.argb(1, 255, 0, 0));
-        //green.setVisibility(View.INVISIBLE);
-        //green.setColorFilter(5);
-        //listaSensori.get(0).getVistaFiglia().statusLed.setColorFilter(Color.argb(1, 255, 0, 0));
-
         a = MainActivity.this;
     }
 
@@ -282,15 +150,13 @@ public class MainActivity extends AppCompatActivity {
         }
         //	should	prompt	user	to	open	settings	if	Bluetooth	is	not	enabled.
         if (mBleCommManager.getmBluetoothAdapter().isEnabled()) {
-            // aggiorno la UI, non serve nel mio programma
-            // onBluetoothActive();
+            // Update bluetooth UI
         } else {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
         mBleCommManager.setmBluetoothLeScanner();
-
     }
 
     public void startScan(){
@@ -335,7 +201,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBlePeripheralDiscovered(final BluetoothDevice bluetoothDevice){
         Log.v(TAG,	"Found "+bluetoothDevice.getName()+", " +	bluetoothDevice.getAddress());
-        //updateTextView("SCOPETO");
         //	only	add	the	peripheral	if
         //	-	it	has	a	name,	on
         //	-	doesn't	already	exist	in	our	list,	or
@@ -367,8 +232,6 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable(){
                 @Override
                 public void run(){
-                    updateTextView("AGGIORNA");
-                    updateTextView(bluetoothDevice.getName()+"\n"+bluetoothDevice.getAddress());
                     mBlePeripheralsListAdapter.notifyDataSetChanged();
                 }
             });
@@ -380,11 +243,9 @@ public class MainActivity extends AppCompatActivity {
         public void onScanResult(int callbackType, ScanResult result){
             BluetoothDevice bluetoothDevice = result.getDevice();
             onBlePeripheralDiscovered(bluetoothDevice);
-            //updateTextView("DEVICE TROVATO "+bluetoothDevice.getAddress());
         }
         @Override
         public	void	onBatchScanResults(List<ScanResult> results){
-            updateTextView("BATCH");
             for	(ScanResult result : results){
                 BluetoothDevice	bluetoothDevice	=	result.getDevice();
                 onBlePeripheralDiscovered(bluetoothDevice);
@@ -392,7 +253,6 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public void onScanFailed(int errorCode){
-            updateTextView("ERRORE");
             switch(errorCode){
                 case SCAN_FAILED_ALREADY_STARTED:
                     Log.e(TAG,"Scan	with	the	same	settings	already	started");
@@ -416,7 +276,6 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable(){
                 @Override
                 public void run(){
-                    updateTextView("SCAN COMPLETATA");
                     onBleScanStopped();
                 }
             });
@@ -426,7 +285,6 @@ public class MainActivity extends AppCompatActivity {
     public final BleScanCallbackv18 mBleScanCallbackv18	= new BleScanCallbackv18(){
                 @Override
                 public void onLeScan(BluetoothDevice bluetoothDevice, int rssi, byte[] scanRecord){
-                    updateTextView(bluetoothDevice.getName());
                     onBlePeripheralDiscovered(bluetoothDevice);
                 }
                 @Override
@@ -460,8 +318,7 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                // aggiorno la UI, non serve nel mio programma
-                                // onBluetoothActive();
+                                // Update bluetooth UI
                             }
                         });
                         break;
@@ -486,16 +343,5 @@ public class MainActivity extends AppCompatActivity {
         if (!listPermissionsNeeded.isEmpty()) {
             ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), MULTIPLE_PERMISSIONS);
         }
-    }
-
-    public void updateTextView(final String toThis) {
-        /*runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                textView.append(toThis + "\n");
-                Editable editable = textView.getEditableText();
-                Selection.setSelection(editable, editable.length());
-            }
-        });*/
     }
 }
